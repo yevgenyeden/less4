1ea83b1f2507a863a51623c00c4ad512f1a73b08
yevgenyeden <41266483+yevgenyeden@users.noreply.github.com>
2018-08-12 18:57:55 +0300

    String to string

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 
 public class hello {
-	public static void main(String[] args  ) {
+	public static void main(string[] args  ) {
 		for (int i=1; i<=10; i++)
 			System.out.println("hello world ..." + i);
 	}
